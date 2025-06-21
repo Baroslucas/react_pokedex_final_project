615295453b003eaa5f3a2dbcb4bdca440ed9af9e
Baroslucas <lucas.baros41@gmail.com>
2025-06-21 16:48:32 +0200

    feat(Reviews): Add api and logics to display a new review with input into Pokemon Review

--- src/api/pokemonApi.js
+++ src/api/pokemonApi.js
@@ -9,3 +9,24 @@ export async function fetchPokemonReview(id) {
   );
   return await response.json();
 }
+
+//
+export async function addReview({ pokemonId, author, content }) {
+  const response = await fetch("http://localhost:3001/reviews/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      pokemonId,
+      author,
+      content,
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error("La review n'a pas été correctement envoyée");
+  }
+
+  return await response.json();
+}
--- src/components/PokemonReview/PokemonReview.jsx
+++ src/components/PokemonReview/PokemonReview.jsx
@@ -2,17 +2,32 @@ import like from '../../assets/img/like.svg'
 import { Review } from '../Review/Review'
 import { useParams } from 'react-router-dom'
 import { useEffect, useState } from "react"
-import { fetchPokemonReview } from '../../api/pokemonApi'
+import { addReview, fetchPokemonReview } from '../../api/pokemonApi'
 
 export function PokemonReview() {
 
     const { id } = useParams()
     const [reviews, setReviews] = useState([])
+    const [newReview, setNewReview] = useState('')
 
     useEffect(() => {
         fetchPokemonReview(id)
             .then(setReviews)
     }, [id])
+
+    const handleSubmit = async () => {
+        if (!newReview) return;
+
+        await addReview({
+            pokemonId: (id),
+            author: "Me",
+            content: newReview,
+        }) 
+
+        const updatedReview = await fetchPokemonReview(id)
+        setReviews(updatedReview)
+        setNewReview('')
+    }
 
     return (
         <div>
@@ -31,6 +46,13 @@ export function PokemonReview() {
                 <input 
                     type="text"
                     placeholder='Add a review ...'
+                    value={newReview}
+                    onChange={(e) => setNewReview(e.target.value)}
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                            handleSubmit()
+                        }
+                    }}
                 />
                 {reviews.map((review) => (
                     <Review 
